d4ea4cb2533e8d9581a1db6c7f87c63156b37b05
Jonathan Glassey <jono@shuttlerock.com>
2016-07-19 22:40:54 +0900

    Add CommentTile Vue

---
+++ src/CommentTile.vue
@@ -0,0 +1,29 @@
+<template class='commentTile'>
+  <div class='comment tile'>
+    <div>
+      <div>{{ comment.details.content }}</div>
+      <div class='commentAuthor'>
+        <img v-bind:src='comment.author.picture'/>
+        <span>{{comment.author.name}}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    comment: {
+      type: Object,
+      default() {
+        return {  }
+      }
+    }
+  },
+  methods: {
+  },
+  ready() {
+
+  }
+ }
+</script>
--- src/app.vue
+++ src/app.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="widgetWrapper">
+  <div class="widgetWrapper" v-if="this.entries.length > 0 && this.site != null">
     <div class='currentImageWrapper'>
-      <img src="{{entry.images.square}}" ng-if="{{entry != null}}" v-show="!isTransitioning" transition='fade'/>
+      <img v-bind:src="entry.images.square" v-show="!isTransitioning" transition='fade'/>
       <div class='currentImageDescription' v-show="!isTransitioning" transition='fade'>
-        {{entry.details.html_description}}
+        {{entry.details.description}}
       </div>
       <div class='currentImageTitle' v-show="!isTransitioning" transition='fade'>
         {{entry.details.name}}
@@ -11,15 +11,19 @@
     </div>
     <div class='rightTiles'>
       <div class='authorInformation tile' >
-        <img src="{{entry.author.picture}}" />
-        <span>{{entry.author.name}}</span>
-        <span>Posted to {{entry.source.network}} {{entry.details.age}}
+        <img v-bind:src="entry.author.picture" />
+        <div class='authorName'>
+          <span>{{entry.author.name}}</span>
+        </div>
       </div>
-      <div class='comment tile'>{{comments[0].details.content}}</div>
-      <div class='comment tile'>Another Comment!</div>
-      <div class='comment tile'>Hey!</div>
-      <div class='previewImage tile'><img src="{{entries[this.count+1].images.square}}"></div>
-      <div class='previewImage tile'><img src="{{entries[this.count+2].images.square}}"></div>
+      <div class='networkInformation tile'>
+        <img v-bind:src='getPostedServiceImage(entry)' />
+        <span>Posted to {{ site.details.name }} {{ entry.details.age }} ago</span>
+      </div>
+      <comment-tile :comment="comments[0]"></comment-tile>
+      <comment-tile :comment="comments[1]"></comment-tile>
+      <div class='previewImage tile'><img v-bind:src="entries[this.count+1].images.square"></div>
+      <div class='previewImage tile'><img v-bind:src="entries[this.count+2].images.square"></div>
     </div>
     <div class='fixedFooter'>
       <div class='tagsWrapper'>
@@ -35,19 +39,31 @@
 </template>
 
 <script>
+import CommentTile from './commenttile.vue'
+
 export default {
+  components: { CommentTile },
   data () {
      return {
-       count: 1,
+       count: 3,
        pageCount: 1,
        entries: [],
+       comments: [{details: {content: 'test'}}],
        entry: {},
-       isTransitioning: false
+       isTransitioning: false,
+       site: null
      }
    },
    methods: {
      increment () {
        this.count ++;
+     },
+     getSite() {
+       this.$http.get('https://api.shuttlerock.com/v2/news/site.json').then((response) => {
+           this.site = response.data;
+       }, (response) => {
+           console.error('Something went wrong!!');
+       });
      },
      getEntries() {
         // GET /someUrl
@@ -68,21 +84,35 @@ export default {
       });
     },
     nextEntry() {
+      this.startTransition();
+      setTimeout(this.setNextEntry, 250);
+      setTimeout(this.nextEntry, 5000);
+    },
+    setNextEntry() {
       this.count += 1;
       console.log('current entry: ' + this.count)
       console.log('current entries: ' + this.entries.length)
-      this.entry = this.entries[this.count];
       this.loadComment();
-      setTimeout(this.startTransition, 3000);
+      this.entry = this.entries[this.count];
       if (this.count >= (this.entries.length - 2 )) {
         console.log('get more entries')
         this.getEntries();
       }
     },
+    getPostedServiceImage(entry) {
+      switch(entry.source.network) {
+        case 'shuttlerock':
+          return this.site.images.square
+          break;
+        default:
+          //TODO: Add Images for other networks
+          break;
+      }
+    },
     startTransition() {
       console.log('startTransition');
       this.isTransitioning = true;
-      setTimeout(this.endTransition, 2000);
+      setTimeout(this.endTransition, 1000);
     },
     endTransition() {
       console.log('endTransition');
@@ -90,10 +120,12 @@ export default {
     }
    },
    ready() {
-     this.getEntries()
+     this.getSite();
+     this.getEntries();
 
+     this.nextEntry()
      this.ifTransitioning = false;
-     setInterval(this.nextEntry, 5000);
+
    }
 }
 </script>
@@ -114,6 +146,8 @@ body {
   min-width: 60%;
   min-height: 100px;
   float: left;
+  color: #fff;
+  text-shadow: 2px 0 0 #000, -2px 0 0 #000, 0 2px 0 #000, 0 -2px 0 #000, 1px 1px #000, -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000;
 }
 
 .currentImageWrapper img {
@@ -122,20 +156,23 @@ body {
 
 .currentImageWrapper, .rightTiles {
   display: inline-block;
+
 }
 
 .currentImageDescription {
   position: absolute;
-  top: 100px;
-  left: 150px;
-  font-size: 20px;
+  top: 73px;
+  left: 65px;
+  font-size: 12px;
+  width: 400px;
   font-style: italic;
 }
 .currentImageTitle {
-  font-size: 33px;
+  font-size: 20px;
   position: absolute;
   top: 50px;
   left: 50px;
+  width: 400px;
   font-weight: 700;
 }
 .fixedFooter {
@@ -150,8 +187,45 @@ body {
   font-size: 20px;
   padding: 0 5px;
 }
-.authorInformation img{
-  width: 100px
+
+.tile {
+  position: relative;
+}
+
+.tile img{
+  width: 100%
+}
+
+.authorInformation .authorName {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  background: rgba(0,0,0,0.5);
+  height: 20px;
+  color: #fff;
+}
+
+.networkInformation span {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  background: rgba(0,0,0,0.5);
+  color: white;
+}
+
+.authorInformation .authorName span {
+  padding: 5px
+}
+
+.commentAuthor {
+  position:absolute;
+  bottom: 0;
+}
+
+.commentAuthor img {
+  width: 30px;
 }
 
 .rightTiles {
@@ -164,10 +238,6 @@ body {
   width: 50%;
   height: 33%;
   float: left;
-}
-
-.tile.previewImage img{
-  width: 100%
 }
 
 .fade-transition {
